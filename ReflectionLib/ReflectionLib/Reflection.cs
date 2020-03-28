01ec9f27bc2d250641ee3eecc80385a4e6d3529e
William Morelli <morelli186@gmail.com>
2020-03-28 12:35:39 +0100

    Update Reflection.cs

--- ReflectionLib/ReflectionLib/Reflection.cs
+++ ReflectionLib/ReflectionLib/Reflection.cs
@@ -12,11 +12,15 @@ namespace ReflectionLib
         /// <param name="_methodeName">The name of method you want to use</param>
         /// <param name="_flagsToSearch">BindingFlags allow a more specific search</param>
         /// <param name="_paramters">Array of parameters of your method</param>
-        public static void Method(object _objectToAcces, string _methodeName, BindingFlags _flagsToSearch, object[] _paramters = null)
+        public static void Method(object _objectToAcces, string _methodeName, BindingFlags _flagsToSearch = BindingFlags.Default, object[] _paramters = null)
         {
+            if (_objectToAcces == null) throw new Exception("Object you want to access is null. Make sure there is no probleme");
+
             Type _hisType = _objectToAcces.GetType();
 
-            MethodInfo _methode = _hisType.GetMethod(_methodeName);
+            MethodInfo _methode = _hisType.GetMethod(_methodeName, _flagsToSearch);
+
+            if (_methode == null) throw new Exception("Method you want to access is not defined ! Maybe you have mistaken her name ?");
 
             _methode.Invoke(_objectToAcces, _flagsToSearch, null, _paramters, null);
         }
@@ -28,14 +32,17 @@ namespace ReflectionLib
         /// <param name="_propertiesName">The name of property you want to use</param>
         /// <param name="_flagsToSearch">BindingFlags allow a more specific search</param>
         /// <returns></returns>
-        public static PropertyInfo Property(object _objectToAcces, string _propertiesName, BindingFlags _flagsToSearch)
+        public static PropertyInfo Property(object _objectToAcces, string _propertiesName, BindingFlags _flagsToSearch = BindingFlags.Default)
         {
+            if (_objectToAcces == null) throw new Exception("Object you want to access is null. Make sure there is no probleme");
+
             Type _hisType = _objectToAcces.GetType();
 
             PropertyInfo _property = _hisType.GetProperty(_propertiesName, _flagsToSearch);
 
-            return _property;
+            if (_property == null) throw new Exception("Property you want to access is not declared ! Maybe you have mistaken her name ?");
 
+            return _property;
         }
 
 
